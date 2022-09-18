6f6e9d32368fb6fde7bc1783f3ce4c780ede58ba
elisalais <elisa_lais1@hotmail.com>
2022-09-17 22:25:05 -0300

    python decisao encadeada

---
+++ Algoritmos/Python/Explorando_ContaAgua.py
@@ -0,0 +1,13 @@
+consumo = int(input())
+if consumo <= 10:
+    conta = conta + 7
+    contador = conta - 10
+elif consumo <=30:
+    conta = (contador - 30)*1
+    contador = contador - 30
+elif consumo <=70:
+    conta = (contador - 70)*1
+    contador = contador - 30
+
+
+
